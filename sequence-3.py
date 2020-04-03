7768a6b78871bdd3771cfde0ed2796098effe1fb
Jeff Cook <jecook2692@gmail.com>
2020-04-03 15:44:38 -0400

    adds odd numbers

--- sequence-3.py
+++ sequence-3.py
@@ -5,3 +5,15 @@ number = 0
 
 #create a "for" loop that loop through each number starting at 0 and ending at 99.
 for number in range(100):
+
+    #create a variable that will only print out the odd numbers from 0 to 99 using the modulo.
+    is_odd = number % 2 != 0
+
+    #Using the "if" statement, print out all odd numbers.
+    if is_odd:
+
+        #Print odd numbers.
+        print(number)
+    
+
+
