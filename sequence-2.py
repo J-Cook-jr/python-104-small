4f7b66c1931b6ba2a75e0c4e87486f0f06716e8a
Jeff Cook <jecook2692@gmail.com>
2020-04-03 15:26:30 -0400

    adds even numbers

--- sequence-2.py
+++ sequence-2.py
@@ -5,5 +5,14 @@ number = 0
 
 #create a "for" loop that loop through each number starting at 0 and ending at 99.
 for number in range(100):
+
+    #create a variable that will only print out the even numbers from 0 to 99 using the modulo.
+    is_even = number % 2 == 0
     
+    #Using the "if" statement, print out all even numbers.
+    if is_even:
+        
+        #Print even numbers.
+        print(number)
+
 
